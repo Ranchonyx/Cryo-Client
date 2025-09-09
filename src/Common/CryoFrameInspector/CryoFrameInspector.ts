ef3138eeb2c46244a369a201c88eb588e077962d
Ranchonyx <felixjanetzki@gmail.com>
2025-09-09 16:07:40 +0200

    unfucked the fucked up typeToStringMap in CryoFrameInspector.ts

--- src/Common/CryoFrameInspector/CryoFrameInspector.ts
+++ src/Common/CryoFrameInspector/CryoFrameInspector.ts
@@ -1,14 +1,14 @@
 import CryoFrameFormatter from "../CryoBinaryMessage/CryoFrameFormatter.js";
 
 const typeToStringMap = {
-    1: "ack",
-    3: "error",
-    2: "ping/pong",
     0: "utf8data",
+    1: "ack",
+    2: "ping/pong",
+    3: "error",
     4: "binarydata",
     5: "server_hello",
     6: "client_hello",
-    7: "handshake_done"
+    7: "handshake_done",
 }
 
 export class CryoFrameInspector {
